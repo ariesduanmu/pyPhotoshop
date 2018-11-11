cd2c192ca94fae10f6a0dc08c5d2432f224b483a
ariesduanmu <aries.duanmu@gmail.com>
2018-11-11 19:26:46 +0800

    change target color to modification function

--- replacecolor.py
+++ replacecolor.py
@@ -3,22 +3,24 @@ import numpy as np
 from PIL import Image
 from scipy.spatial.distance import cdist
 
-def replace_corlor(image, original_corlor, target_corlor, distance=10, output="new_test.jpg"):
-    print("[*] START")
+def replace_corlor(image, original_corlor, modification, distance=10, output="new_test.jpg"):
+    print("[*] START Replace Color")
     img = Image.open(image)
     data = np.asarray(img, dtype="int32")
-    r, g, b = original_corlor
     w,h,k = data.shape
     data = np.reshape(data, (w*h,k))
     distMatrix = cdist(data, np.array([original_corlor]))
     D = distMatrix<=distance
     D = np.reshape(D,w*h)
-    data[:,:k][D] = target_corlor
+    data[:,:k][D] = modification(data[:,:k][D])
     data = np.reshape(data, (w,h,k))
     img = Image.fromarray(np.asarray(np.clip(data, 0, 255), dtype="uint8"), "RGB")
     img.save(output)
-    print("[*] DONE")
+    print("[*] DONE Replace Color")
 
 if __name__ == "__main__":
-    replace_corlor("test.jpg",[36,35,30],(255,0,0))
+    def modification(color):
+        return color + [50,0,0]
+        # return [255,0,0]
+    replace_corlor("test.jpg",(36,35,30),modification)
 
